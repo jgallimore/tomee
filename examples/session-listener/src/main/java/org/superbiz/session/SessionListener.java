088b3b6d43cc011f3f04dbf2fc4509ca349d92b9
Jonathan Gallimore <jon@jrg.me.uk>
2019-01-08 15:38:36 +0000

    Track session creation

--- examples/session-listener/src/main/java/org/superbiz/session/SessionListener.java
+++ examples/session-listener/src/main/java/org/superbiz/session/SessionListener.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpSessionListener;
 public class SessionListener implements HttpSessionListener {
     @Override
     public void sessionCreated(final HttpSessionEvent se) {
-        // NO-OP
+        Sessions.sessionCreated(se.getSession().getId());
     }
 
     @Override
